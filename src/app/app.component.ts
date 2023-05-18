63ffb22087a2ad8f3a3b648afcdaf073b6385ace
Bianca <bruninhakjurugat11@gmail.com>
2023-05-18 15:25:42 -0300

    feat: sign message and get token balances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MetamaskService } from './services/metamask.service';
+import { AlchemyService } from './services/alchemy.service';
+import { TokenBalance } from 'alchemy-sdk';
+import { FormControl, Validators } from '@angular/forms';
 
 declare global {
   interface Window {
@@ -18,15 +21,35 @@ export class AppComponent {
   currentAccount$ = this.metamaskService.currentAccount$;
   balance$ = this.metamaskService.balance$;
   hasMetamask;
+  tokenBalances: TokenBalance[] = [];
+  message = new FormControl('', Validators.required);
+  signatures: string[] = [];
 
-  constructor(private metamaskService: MetamaskService) {
+  constructor(
+    private metamaskService: MetamaskService,
+    private alchemyService: AlchemyService
+  ) {
     this.hasMetamask = metamaskService.checkMetamaskAvailability();
     if (this.hasMetamask) {
       metamaskService.retrieveConnection();
     }
+    this.currentAccount$.subscribe(async (account) => {
+      if (account) {
+        this.tokenBalances = await this.alchemyService.getTokenBalances(
+          account
+        );
+      }
+    });
   }
 
   connectWallet() {
     this.metamaskService.connectWallet();
+  }
+
+  signMessage() {
+    const message = this.message.value!;
+    this.metamaskService.signer?.signMessage(message).then((signature) => {
+      this.signatures.push(signature);
+    });
   }
 }
