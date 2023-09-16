0d0f5b93f7273ff64eec7d61bd7cb03c130e7998
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-09-16 14:18:33 -0300

    feat: connect wallet wagmi component

---
+++ src/app/connect-wallet-wagmi/connect-wallet-wagmi.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { getAccount } from '@wagmi/core';
+import { Subscription } from 'rxjs';
+import { WalletService } from '../services/wallet.service';
+
+type HexString = `0x${string}` | undefined;
+
+@Component({
+  selector: 'app-connect-wallet-wagmi',
+  templateUrl: './connect-wallet-wagmi.component.html',
+  styleUrls: ['./connect-wallet-wagmi.component.css'],
+})
+export class ConnectWalletWagmiComponent {
+  accountSubscription: Subscription = new Subscription();
+  walletAddress: HexString;
+  walletBalance: string = '0';
+  walletTokens: string = '0';
+  step = 1;
+  message: string;
+  signatures: string[] = [];
+
+  constructor(private walletService: WalletService) {
+    this.message = '';
+  }
+
+  ngOnInit() {
+    const currentAccount = getAccount();
+    if (currentAccount && currentAccount.isConnected) {
+      this.step = 2;
+      this.walletAddress = currentAccount.address;
+    } else {
+      this.step = 1;
+    }
+    this.accountSubscription = this.walletService.accountChanged$.subscribe({
+      next: async (account) => {
+        if (account.isConnected) {
+          if (account.isConnected) {
+            this.step = 2;
+            this.walletAddress = account.address;
+            this.walletBalance = await this.walletService.getWalletBalance();
+            // this.walletTokens = await this.walletService.getTokenBalances();
+          } else {
+            this.step = 1;
+          }
+        }
+      },
+      error: (err) => {
+        console.error('Error obtaining wallet connection status: ', err);
+      },
+    });
+  }
+
+  connectWallet() {
+    this.walletService.web3Modal.openModal();
+  }
+
+  ngOnDestroy() {
+    this.accountSubscription.unsubscribe();
+  }
+
+  signMessageForm() {
+    this.walletService.signMessage(this.message).then((signature) => {
+      this.signatures.push(signature);
+    });
+  }
+}
