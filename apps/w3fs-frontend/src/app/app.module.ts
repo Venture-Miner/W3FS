da12087bcf410196e4243c7b60de5f1144902fe6
Matheus Pagani <matheus@ventureminer.com>
2023-11-29 14:34:20 -0300

    Cleanup

--- apps/w3fs-frontend/src/app/app.module.ts
+++ apps/w3fs-frontend/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnectWalletWagmiComponent } from './connect-wallet-wagmi/connect-wallet-wagmi.component';
-import { ConnectWalletAlchemyComponent } from './connect-wallet-alchemy/connect-wallet-alchemy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectWalletWagmiComponent,
-    ConnectWalletAlchemyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
